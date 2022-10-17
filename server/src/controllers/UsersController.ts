47041dee0b82f90085e4c0c523fab197fec02fa7
Luciane Santos <lucianesantcs@gmail.com>
2022-10-17 19:50:38 -0300

    feat: criado método para pegar todos os usuários

--- server/src/controllers/UsersController.ts
+++ server/src/controllers/UsersController.ts
@@ -3,6 +3,13 @@ import connectDB from "../database/connection";
 import Users from "../models/Users";
 
 export default {
+  async index(request: Request, response: Response) {
+    const usersRepository = connectDB.getRepository(Users);
+    const users = await usersRepository.find();
+
+    return response.json(users);
+  },
+
   async create(request: Request, response: Response) {
     const { name, username, password, email, birthdate } = request.body;
     const usersRepository = connectDB.getRepository(Users);
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -3,6 +3,7 @@ import UsersController from "./controllers/UsersController";
 
 const routes = Router();
 
+routes.get("/users", UsersController.index);
 routes.post("/users", UsersController.create);
 
 export default routes;
