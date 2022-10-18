4cd0fbbbf332ac1bcd0259b491094b177bec103b
Luciane Santos <lucianesantcs@gmail.com>
2022-10-17 21:00:02 -0300

    feat: criado arquivo para lidar com erros

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,11 @@
 import express from "express";
-import routes from './routes';
+import "express-async-errors";
+import errorHandler from "./errors/handler";
+import routes from "./routes";
 
 const app = express();
 app.use(express.json());
 app.use(routes);
+app.use(errorHandler);
 
 app.listen(3333);
