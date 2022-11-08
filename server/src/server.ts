1e6d94812f4e6181438e65ca9b3faab706833f35
Luciane Santos <lucianesantcs@gmail.com>
2022-11-08 19:30:34 -0300

    build: instalado lib cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,9 +2,11 @@ import express from "express";
 import "express-async-errors";
 import errorHandler from "./errors/handler";
 import routes from "./routes";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 app.use(routes);
 app.use(errorHandler);
 
